6d890ef884d056f13f45b51fc17acc2201752d8a
Marta Mazelanik <marta.mazelanik@astrazeneca.com>
2022-10-27 10:15:18 +0200

    Add new preconditions to the function

--- inflammation/models.py
+++ inflammation/models.py
@@ -48,15 +48,19 @@ def patient_normalise(data):
     """
     Normalise patient data froma 2D inflammation data array.
 
-    NaN values are ignored, and normalised to 0.
+    Aby NaN values are ignored, and normalised to 0.
 
-    Negative values are rounded to 0.
+    :param data: 2D array of inflammation data
+    :type data: ndarray
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('Inflammation input should be an ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('Inflammation array should be 2-dimensional')
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
-    max = np.max(data, axis=1)
+    max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
-    normalised[normalised < 0] = 0
     return normalised
