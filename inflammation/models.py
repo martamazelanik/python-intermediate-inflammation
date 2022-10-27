453af5fd81a7df922db8942fc12fc347b8ac41da
Marta Mazelanik <marta.mazelanik@astrazeneca.com>
2022-10-27 10:38:29 +0200

    Rename local variables from max to max_data

--- inflammation/models.py
+++ inflammation/models.py
@@ -59,8 +59,8 @@ def patient_normalise(data):
         raise ValueError('Inflammation array should be 2-dimensional')
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
-    max = np.nanmax(data, axis=1)
+    max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     return normalised
